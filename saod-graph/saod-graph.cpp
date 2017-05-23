5f8dd07bd8f1ebc1b7808ee1fb2898156bc71b5b
avm <avm@AVM_NOTE>
2017-05-23 22:52:26 +0300

    sdfesdfsgfsd

--- saod-graph/saod-graph.cpp
+++ saod-graph/saod-graph.cpp
@@ -12,25 +12,33 @@ using namespace std;
 class Graph {
 
 public:
-	struct graph_verge {
-		graph_verge(int A, int B, double weight) : node_A(A), node_B(B), verge_weight(weight) { };
-		int node_A;
-		int node_B;
-		double verge_weight;
-		bool operator < (const graph_verge & second) { return (node_A < second.node_A) || (node_A == second.node_A && node_B < second.node_B); };
-		bool operator == (const graph_verge & second) { return node_A == second.node_A && node_B == second.node_B; };
-	};
-
-	struct graph_node {
-		graph_node(int idx, double weight = 0) : node_idx(idx), node_weight(weight) { };
+	struct node {
 		int node_idx;
 		double node_weight;
-		bool operator < (const graph_node & second) { return node_idx < second.node_idx; };
-		bool operator == (const graph_node & second) { return node_idx == second.node_idx; };
+		node(int idx, double weight = 0) : node_idx(idx), node_weight(weight) { };
+		bool operator < (const node & second) { return node_idx < second.node_idx; };
+		bool operator == (const node & second) { return node_idx == second.node_idx; };
 	};
 
-	typedef list<graph_verge> Verges;
-	typedef list<graph_node> Nodes;
+	struct verge {
+		node* node_A;
+		node* node_B;
+		double verge_weight;
+		verge(node* A, node* B, double weight) : node_A(A), node_B(B), verge_weight(weight) { };
+		verge(int A, int B, double weight) : node_A(new node(A)), node_B(new node(B)), verge_weight(weight) { };
+		bool operator < (const verge & second) { 
+			return (node_A->node_idx < second.node_A->node_idx) 
+				|| (node_A->node_idx == second.node_A->node_idx 
+					&& node_B->node_idx < second.node_B->node_idx);
+		};
+		bool operator == (const verge & second) { 
+			return node_A->node_idx == second.node_A->node_idx 
+				&& node_B->node_idx == second.node_B->node_idx; 
+		};
+	};
+
+	typedef list<verge> Verges;
+	typedef list<node> Nodes;
 	typedef std::map<int, std::map<int, double>> ConnMap;
 
 private:
@@ -48,27 +56,46 @@ public:
 	Graph() {};
 	Graph(Graph const &G) = delete; // Удаляем конструктор копирования
 	Graph(Graph &&G) = delete; // Удаляем конструктор перемещения
-	void *operator new(std::size_t) = delete; // Удаляем оператор new
+	void *operator new(std::size_t) = delete; // Удаляем операторы new
 	void *operator new[](std::size_t) = delete; //
 	
-	void Verges_Add(graph_node A, graph_node B, double weight);
-	void Verges_Add(int A, int B, double weight) { Verges_Add(graph_node(A), graph_node(B), weight); };
-	bool Verges_Del(int A, int B);
-	std::shared_ptr<graph_verge> Get_Verge(int A, int B);
+	void Verges_Add(node *A, node *B, double weight) {
 
-	bool Nodes_Add(graph_node node) {
-		bool node_exist = false;
-		for (auto iter = _nodes.begin(); iter != _nodes.end(); iter++)
-			if (iter->node_idx == node.node_idx)
-				node_exist = true;
-		if (node_exist)
-			return false;
-		else {
+	};
+
+	void Verges_Add(int A, int B, double weight) { Verges_Add(new node(A),new node(B), weight); };
+
+	bool Verges_Del(int A, int B);
+
+	std::shared_ptr<verge> Get_Verge(int A, int B);
+
+	node * Get_Node(int idx) {
+		Graph::Nodes::iterator iter = std::find(_nodes.begin(), _nodes.end(), node(idx));
+		if (iter != std::end(_nodes))
+			return &(*iter);
+		else
+			return nullptr;
+	};
+
+	node * Get_Node(node &_node) {
+		Graph::Nodes::iterator iter = std::find(_nodes.begin(), _nodes.end(), _node);
+		if (iter != std::end(_nodes))
+			return &(*iter);
+		else
+			return nullptr;
+	};
+
+	bool Nodes_Add(node &node) {
+		if (Get_Node(node.node_idx)) {
 			_nodes.push_back(node);
 			return true;
+		} else {
+			return false;
 		}
 	};
-	bool Nodes_Add(int idx) { return Nodes_Add(graph_node(idx)); };
+
+	bool Nodes_Add(int idx) { return Nodes_Add(node(idx)); };
+
 	bool Nodes_Del(int idx) {
 		for (auto iter = _nodes.begin(); iter != _nodes.end(); iter++)
 			if (iter->node_idx == idx) {
@@ -78,23 +105,14 @@ public:
 		return false;
 	};
 
-	graph_node * Get_Node(int idx) {
-		Graph::Nodes::iterator iter = std::find(_nodes.begin(), _nodes.end(), graph_node(idx));
-		if (iter != std::end(_nodes)) 
-			return &(*iter);
-		else 
-			return nullptr;
-	};
-
 	void Print_Verges();
+
 	void Print_Nodes() {
 		for (const auto &it : _nodes) 
 			cout << it.node_idx << "(" << it.node_weight << ")\t";
 		cout << endl;
-		//for (auto iter = _nodes.begin(); iter != _nodes.end(); iter++)
-		//	cout << iter->node_idx << "(" << iter->node_weight << ")\t";
-		//cout << endl;
 	};
+
 	void Print_Connectivity_Matrix(); // Выводим граф в виде матрицы смежности
 
 };
@@ -103,29 +121,30 @@ int main()
 {
 	//Graph* ptr = new Graph;
 	
-	Graph::graph_verge v1(1, 3, 10.0);
-	Graph::graph_verge v2(1, 2, 10.0);
-	Graph::graph_verge v3(2, 1, 10.0);
-	Graph::graph_verge v4(1, 1, 10.0);
-	Graph::graph_verge v5(1, 3, 20.0);
+	Graph::verge v1(1, 3, 10.0);
+	Graph::verge v2(1, 2, 10.0);
+	Graph::verge v3(2, 1, 10.0);
+	Graph::verge v4(1, 1, 10.0);
+	Graph::verge v5(1, 3, 20.0);
 
 	cout << (v1 < v2) << endl;
 	cout << (v1 == v2) << endl;
 	cout << (v1 == v5) << endl;
 
-
 	Graph g1;
 	g1.Nodes_Add(1);
 	g1.Nodes_Add(3);
 	g1.Nodes_Add(5);
-	g1.Nodes_Add(Graph::graph_node(7, 10));
+	g1.Nodes_Add(Graph::node(7, 10));
 	cout << "Printing nodes: " << endl;
 	g1.Print_Nodes();
 
 	//std::shared_ptr<Graph::graph_node> node_ptr = g1.Get_Node(1);
-	Graph::graph_node * node = g1.Get_Node(1);
+	Graph::node * node = g1.Get_Node(1);
 	if (node) 
 		cout << "node found = " << node->node_idx << "(" << node->node_weight << ")" << endl;
+
+
 
 	cout << endl << "\n\nEnter x to exit...";
 	char a;
